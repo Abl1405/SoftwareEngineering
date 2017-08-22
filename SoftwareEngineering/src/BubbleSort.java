cd3a59f126f0d8d3081cbc4af618ce50bcb0ca73
Abl1405 <fancyfire.yash@gmail.com>
2017-08-22 12:02:57 +0530

    Updated

--- SoftwareEngineering/src/BubbleSort.java
+++ SoftwareEngineering/src/BubbleSort.java
@@ -1,4 +1,20 @@
 
 public class BubbleSort {
-
+	public static int[] arr= {5,3,6,2,1};
+	public static void main(String args[]) {
+		for(int i=0;i<arr.length;i++) {
+			for(int j=i;j<arr.length;j++) {
+				if(arr[j]<arr[i]) {
+					arr[i]=arr[i]+arr[j];
+					arr[j]=arr[i]-arr[j];
+					arr[i]=arr[i]-arr[j];
+				}
+			}
+		}
+		for(int i=0; i<arr.length;i++) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+	
 }
+
